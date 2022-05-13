7c9b6fd693b61b75cc716f77b80fc83075a1d116
ersidimatranxhi <ersidi.matranxhi1@gmail.com>
2022-05-13 18:45:59 +0200

    fine esercizio, non funziona il tasto SALVA.

--- WebAppPiezzeria/Controllers/PizzaController.cs
+++ WebAppPiezzeria/Controllers/PizzaController.cs
@@ -59,11 +59,99 @@ namespace WebAppPiezzeria.Controllers
             PizzaData.GetPizze().Add(nuovaPizza);
             return RedirectToAction("Index");
         }
-    
+
+
+
+        [HttpGet]
+        public IActionResult ModificaPizza(int id)
+        {
+            Pizza pizza = null;
+
+            foreach (Pizza OgniPizza in PizzaData.GetPizze())
+            {
+                if (OgniPizza.Id == id)
+                {
+                    pizza = OgniPizza;
+                    break;
+
+                }
+            }
+      
+        if(pizza == null)
+            {
+                return NotFound();
+            }
+        else
+            {
+                return View("Modifica", pizza);
+            }
+        }
+        
+        
+        
+        
+        [HttpPost]
+        public IActionResult ModificaPizza(int id, Pizza pizzaModificata)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Modifica", pizzaModificata);
+            }
+        Pizza pizzaDaModificare = null;
+
+            foreach (Pizza pizza in PizzaData.GetPizze())
+            {
+                if (pizza.Id == id)
+                {
+                    pizzaDaModificare = pizza;
+                    break;
+                }
+            }
+            if (pizzaDaModificare != null)
+            {
+                pizzaDaModificare.Nome = pizzaModificata.Nome;
+                pizzaDaModificare.Descrizione = pizzaModificata.Descrizione;
+                pizzaDaModificare.Prezzo = pizzaModificata.Prezzo;
+                pizzaDaModificare.Img = pizzaModificata.Img;
+            }
+            else
+            {
+                return NotFound();
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        public IActionResult EliminaPizza(int id)
+        {
+            int indiceDaTrovare = -1;
+            List<Pizza> listaPizza = PizzaData.GetPizze();
+            for(int i = 0; i<listaPizza.Count; i++)
+            {
+                if (listaPizza[i].Id == id)
+                {
+                    indiceDaTrovare = i;
+                }
+            
+            
+            }
+        if(indiceDaTrovare == -1)
+            {
+                return NotFound();
+            }
+            else
+            {
+                PizzaData.GetPizze().RemoveAt(indiceDaTrovare);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
     }
-
-
-
 
 
 }
